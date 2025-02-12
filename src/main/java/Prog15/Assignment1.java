6775085da8bad6e2914c9bd566551447755ec35d
Daniel Morales <55546610+damora-code@users.noreply.github.com>
2025-02-11 23:39:08 -0500

    isPrime DA and RA done, check with professor

--- src/main/java/Prog15/Assignment1.java
+++ src/main/java/Prog15/Assignment1.java
@@ -11,7 +11,7 @@ import java.util.Random;
 
 public class Assignment1 {
     BigInteger FIRST = new BigInteger("10000000000000819");
-    BigInteger TEST = new BigInteger("15");
+    BigInteger TEST = new BigInteger("561");
     BigInteger ZERO = BigInteger.ZERO;
     BigInteger ONE = BigInteger.ONE;
     BigInteger TWO = BigInteger.TWO;
@@ -19,7 +19,8 @@ public class Assignment1 {
 
 
     public Assignment1() {
-        System.out.println(isPrimeDeterministic(TEST));;
+        System.out.println(isPrimeDeterministic(TEST));
+        System.out.println(isProbablyPrimeRandomized(TEST,10));
     }
 
     public static void main(String[] args) {
@@ -30,8 +31,8 @@ public class Assignment1 {
      * This is the primality test used in Prog15_05 from main and the isPrime function, modified to
      * be used in a single function, where we implement a deterministic approach
      *
-     * @param n - the number that will be tested if it is prime
-     * @return - will return true if number is prime, false if not
+     * @param n the BigInteger that will be tested if it is prime
+     * @return will return true if number is prime, false if not
      */
     public boolean isPrimeDeterministic(BigInteger n) {
         BigInteger s = n.sqrt();
@@ -51,6 +52,39 @@ public class Assignment1 {
         }
 
         return true;
+    }
+
+    /**
+     * This is a primality test used in Prog15_06 from the isProbablyPrime function, modified to use
+     * Fermat's Little Theorem using random bases to check if it satisfies the theorem
+     *
+     * @param n the BigInteger that will be tested if it is prime
+     * @param iterations how many times it will run with different random bases
+     * @return will return true if number is prime, false if not
+     */
+    public boolean isProbablyPrimeRandomized(BigInteger n, int iterations) {
+        if (n.compareTo(ONE) <= 0) { // if 1, not prime
+            return false;
+        } else if (n.compareTo(TWO) == 0 || n.compareTo(BigInteger.valueOf(3)) == 0) { // 2 is smallest prime, 3 is also prime
+            return true;
+        }
+
+        Random random = new Random();
+
+        for (int i = 0; i < iterations; i++) {
+            BigInteger a;
+
+            // this helps create a random base in range [2, n-2]
+            do {
+                a = new BigInteger(n.bitLength(), random);
+            } while (a.compareTo(TWO) < 0 || a.compareTo(n.subtract(ONE)) >= 0);
+
+            // -- CRITICAL: USE FORMULA FROM CLASS a^(n-1) ≡ 1 (mod n) --
+            if (!a.modPow(n.subtract(ONE), n).equals(ONE)) {
+                return false; // composite number
+            }
+        }
+        return true; // n is probably prime
     }
 
     /**
