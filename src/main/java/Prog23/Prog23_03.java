ac207ac5b650e09b58fb0377562d657469dfea2d
danny <daniel.morales117@yahoo.com>
2025-04-16 19:31:23 -0400

    added more

---
+++ src/main/java/Prog23/Prog23_03.java
@@ -0,0 +1,34 @@
+package Prog23;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+public class Prog23_03 {
+
+    public Prog23_03() {
+
+        Graph<String, DefaultEdge> g = new SimpleGraph<>(DefaultEdge.class);
+
+        g.addVertex("0");
+        g.addVertex("1");
+        g.addVertex("2");
+        g.addVertex("3");
+        g.addVertex("4");
+        g.addEdge("0", "1");
+        g.addEdge("0", "2");
+        g.addEdge("1", "2");
+        g.addEdge("1", "3");
+        g.addEdge("2", "3");
+        g.addEdge("2", "4");
+
+        for (String v : g.vertexSet()) {
+            System.out.println("deg(" + v + ") = " + g.degreeOf(v));
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new Prog23_03();
+    }
+}
